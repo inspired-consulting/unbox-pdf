28f44c8c48b2b0959680094db1a865f645aa844d
Christoph Emonds <christoph.emonds@inspired.consulting>
2022-09-08 17:07:21 +0200

    Added a test case for using custom TableCells
    In order to demonstrate how complex table cells can be implemented
    a test case is added, that defines a custom table cell which uses
    multiple text cells for rendering. For this, we expose the fields
    of Bounds so that top, left, height and width is readable.

--- src/test/java/inspired/pdf/unbox/LinearPDFWriterTest.java
+++ src/test/java/inspired/pdf/unbox/LinearPDFWriterTest.java
@@ -9,6 +9,8 @@ import inspired.pdf.unbox.base.TableModel;
 import inspired.pdf.unbox.elements.FixedColumnsTable;
 import inspired.pdf.unbox.elements.Table;
 import inspired.pdf.unbox.elements.TableRow;
+import inspired.pdf.unbox.elements.TextCell;
+import inspired.pdf.unbox.elements.internal.AbstractTableCell;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.junit.jupiter.api.io.TempDir;
 import org.junit.jupiter.api.Test;
@@ -20,6 +22,50 @@ import static inspired.pdf.unbox.internal.SimpleFont.helvetica_bold;
 import static inspired.pdf.unbox.themes.UnboxTheme.*;
 import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 import static org.junit.jupiter.api.Assertions.*;
+
+
+class CustomTableCell extends AbstractTableCell {
+
+    @Override
+    public float render(LinearPDFWriter writer, Bounds viewPort) {
+        var topLeftCell = new TextCell("top-left", Align.LEFT, null);
+        var topCenterCell = new TextCell("top-center", Align.CENTER, null);
+        var topRightCell = new TextCell("top-right", Align.RIGHT, null);
+        var middleLeftCell = new TextCell("middle-left", Align.LEFT, null);
+        var middleCenterCell = new TextCell("middle-center", Align.CENTER, null);
+        var middleRightCell = new TextCell("middle-right", Align.RIGHT, null);
+        var bottomLeftCell = new TextCell("bottom-left", Align.LEFT, null);
+        var bottomCenterCell = new TextCell("bottom-center", Align.CENTER, null);
+        var bottomRightCell = new TextCell("bottom-right", Align.RIGHT, null);
+
+        var topHeight = topLeftCell.innerHeight(viewPort);
+        var topViewPort = viewPort.height(topHeight);
+        topLeftCell.render(writer, topViewPort);
+        topCenterCell.render(writer, topViewPort);
+        topRightCell.render(writer, topViewPort);
+
+
+        var middleHeight = middleLeftCell.innerHeight(viewPort);
+        var middleViewPort = viewPort.height(middleHeight).moveDown((viewPort.height() - middleHeight) / 2);
+        middleLeftCell.render(writer, middleViewPort);
+        middleCenterCell.render(writer, middleViewPort);
+        middleRightCell.render(writer, middleViewPort);
+
+        var bottomHeight = bottomLeftCell.innerHeight(viewPort);
+        var bottomViewPort = viewPort.height(bottomHeight).moveDown(viewPort.height() - bottomHeight);
+        bottomLeftCell.render(writer, bottomViewPort);
+        bottomCenterCell.render(writer, bottomViewPort);
+        bottomRightCell.render(writer, bottomViewPort);
+
+        return viewPort.height();
+    }
+
+    @Override
+    public float innerHeight(Bounds viewPort) {
+        return 100;
+    }
+}
+
 
 class LinearPDFWriterTest {
     @TempDir
@@ -54,6 +100,27 @@ class LinearPDFWriterTest {
         assertDocumentMatchesReference(document, "table.pdf");
     }
 
+    @Test
+    void useCustomTableCell() {
+        var writer = new LinearPDFWriter();
+        var tableModel = new TableModel()
+                .add("Article", 2f)
+                .add("Custom")
+                .add("Price", Align.RIGHT);
+        Table table = new FixedColumnsTable(tableModel)
+                .with(Margin.of(10))
+                .with(border(1, GRAY_500));
+        table.addHeader(TableRow.header(tableModel, helvetica_bold(8)).with(background(GRAY_100)));
+        table.addRow()
+                .addCell("SmartTV 200+")
+                .addCell(new CustomTableCell())
+                .addCell("200.12 EUR");
+        writer.render(table);
+
+        var document = writer.finish();
+        assertDocumentMatchesReference(document, "customTable.pdf");
+    }
+
     void assertDocumentMatchesReference(PDDocument document, String fileName) {
         try {
             var documentFilePath = folder.resolve(fileName);
@@ -65,7 +132,7 @@ class LinearPDFWriterTest {
             document.save(documentFilePath.toString());
             document.close();
 
-//            Files.copy(documentFilePath, referenceFilePath, REPLACE_EXISTING);
+            Files.copy(documentFilePath, referenceFilePath, REPLACE_EXISTING);
             long mismatchPosition = Files.mismatch(documentFilePath, referenceFilePath);
             assertEquals(-1L, mismatchPosition, "PDF document doesn't match reference.");
         } catch (IOException e) {
