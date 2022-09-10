c0313d50958275be4c2a3757bee1a40fbee4a67e
Oliver Tigges <oliver.tigges@inspired.consulting>
2022-09-10 14:46:37 +0200

    Added additional creator methods to Border

--- src/main/java/inspired/pdf/unbox/Border.java
+++ src/main/java/inspired/pdf/unbox/Border.java
@@ -1,5 +1,12 @@
 package inspired.pdf.unbox;
 
+/**
+ * Represents a border that can be added to an element.
+ * @param top Thickness of top border
+ * @param right Thickness of right border
+ * @param bottom Thickness of bottom border
+ * @param left Thickness of left border
+ */
 public record Border(float top, float right, float bottom, float left) {
 
     public static Border of(float m) {
@@ -16,6 +23,22 @@ public record Border(float top, float right, float bottom, float left) {
 
     public static Border of(float top, float vertical, float bottom) {
         return new Border(top, vertical, bottom, vertical);
+    }
+
+    public static Border top(float top) {
+        return new Border(top, 0,0,0);
+    }
+
+    public static Border bottom(float bottom) {
+        return new Border(0,0,bottom, 0);
+    }
+
+    public static Border left(float left) {
+        return new Border(0,0,0, left);
+    }
+
+    public static Border right(float right) {
+        return new Border(0,right,0, 0);
     }
 
     public boolean isUniform() {
